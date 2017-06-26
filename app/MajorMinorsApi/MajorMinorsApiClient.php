c674be1c66b33f666b5be99b11e8325c98d80eb0
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-26 21:36:05 +0000

    implement Major-Minors API Client

--- app/MajorMinorsApi/MajorMinorsApiClient.php
+++ app/MajorMinorsApi/MajorMinorsApiClient.php
@@ -2,7 +2,48 @@
 
 namespace App\MajorMinorsApi;
 
+use Zttp\Zttp;
+
 class MajorMinorsApiClient
 {
+    protected $endpoint = '';
+    protected $token = '';
 
+    public function __construct(string $endpoint = null, string $token = null)
+    {
+        if (isset($endpoint)) {
+            $this->endpoint($endpoint);
+        }
+
+        if (isset($token)) {
+            $this->token($token);
+        }
+    }
+
+    public function endpoint(string $endpoint) : MajorMinorsApiClient
+    {
+        $this->endpoint = $endpoint;
+
+        return $this;
+    }
+
+    public function token(string $token) : MajorMinorsApiClient
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    public function get(string $student_id) : array
+    {
+        $query = http_build_query([
+            '1' => $student_id,
+        ]);
+
+        $url = "{$this->endpoint}?$query";
+
+        return (Zttp::withHeaders([
+            'X-Password' => "{$this->token}",
+        ])->get($url))->json();
+    }
 }
--- tests/Unit/MajorMinorsApiTest.php
+++ tests/Unit/MajorMinorsApiTest.php
@@ -11,4 +11,21 @@ class MajorMinorsApiTest extends TestCase
     {
         $this->assertInstanceOf(MajorMinorsApiClient::class, new MajorMinorsApiClient);
     }
+
+    public function test_returns_majors_and_minors()
+    {
+        $port = getenv('TEST_SERVER_PORT');
+        $endpoint = "http://localhost:$port/majorminors";
+        $authtoken = 'AuthToken';
+        $student_id = 900000001;
+
+        $expected = [
+            ['Major' => 'Marketing'],
+            ['Minor' => 'Minor in Finance-Ins/Re'],
+        ];
+
+        $actual = (new MajorMinorsApiClient($endpoint, $authtoken))->get($student_id);
+
+        $this->assertEquals($actual, $expected);
+    }
 }
