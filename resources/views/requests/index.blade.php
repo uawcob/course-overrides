92c504ed8dbe658afa8cb2892ed11ba48d857995
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-29 19:38:28 +0000

    fix missing javascript

--- resources/views/requests/index.blade.php
+++ resources/views/requests/index.blade.php
@@ -11,28 +11,3 @@
         @include('include.cart-items')
     @endif
 @endsection
-
-@push('scripts')
-<script>
-function refreshPlans(){
-    var divPlans = $('#div-plans');
-
-    divPlans.slideUp(400, function(){
-        $('#plans-fetch-error').hide();
-        $.ajax({
-            url: '/plans?html=1&refresh=1',
-            success: function (data) {
-                $('#ul-plans').html(data);
-            },
-            error: function (data) {
-                $('#plans-fetch-error').show();
-                console.log(data);
-            },
-            complete: function() {
-                divPlans.slideDown();
-            }
-        });
-    });
-}
-</script>
-@endpush
