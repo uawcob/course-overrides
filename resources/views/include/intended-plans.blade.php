4a8e05a4d51282ffdb960bed950a05d0930f8e7d
Jeff Puckett <jeff@jeffpuckett.com>
2017-11-29 15:40:55 +0000

    test render intended plans select options

--- resources/views/include/intended-plans.blade.php
+++ resources/views/include/intended-plans.blade.php
@@ -1,34 +1,16 @@
-{{-- @foreach ($intendedPlanOptions as $category => $options)
-<optgroup label="{{ $category }}">
-    @foreach ($options as $option)
-    <option value="{{ $option->id }}">{{ $option->name }}</option>
-    @endforeach
-</optgroup>
-@endforeach --}}
-
-<div class="form-group">
-    <label for="sel-intended-plans-majors">Majors:</label>
-    <select class="form-control" id="sel-intended-plans-majors" name="sel-intended-plans-majors">
-        <option value="1">Economics—Business Economics</option>
-        <option value="4">Economics—International ECON and Business</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
-
-<div class="form-group">
-    <label for="sel-intended-plans-minors-for-business-majors">Minors for Business Majors:</label>
-    <select class="form-control" id="sel-intended-plans-minors-for-business-majors" name="sel-intended-plans-minors-for-business-majors">
-        <option value="2">Finance—Investments/Banking</option>
-        <option value="5">Finance—Real Estate/Insurance</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-minors-for-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
-
-<div class="form-group">
-    <label for="sel-intended-plans-minors-for-non-business-majors">Minors for Non-Business Majors:</label>
-    <select class="form-control" id="sel-intended-plans-minors-for-non-business-majors" name="sel-intended-plans-minors-for-non-business-majors">
-        <option value="3">Information Systems</option>
-        <option value="6">Enterprise Systems</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-minors-for-non-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+@foreach ($intendedPlanOptions as $category => $options)
+    <?php $kebabcat = kebab_case(camel_case($category)); ?>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="sel-intended-plans-{{ $kebabcat }}">{{ $category }}:</label>
+                <select class="form-control" id="sel-intended-plans-{{ $kebabcat }}" name="sel-intended-plans-{{ $kebabcat }}">
+                    @foreach ($options as $option)
+                        <option value="{{ $option->id }}">{{ $option->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <button id='btn-add-intended-plan-{{ $kebabcat }}' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+        </div>
+    </div>
+@endforeach
--- tests/Unit/IntendedPlanSelectTest.php
+++ tests/Unit/IntendedPlanSelectTest.php
@@ -45,32 +45,42 @@ class IntendedPlanSelectTest extends TestCase
             ]);
 
         $options = <<<OPTIONS
-<div class="form-group">
-    <label for="sel-intended-plans-majors">Majors:</label>
-    <select class="form-control" id="sel-intended-plans-majors" name="sel-intended-plans-majors">
-        <option value="1">Economics—Business Economics</option>
-        <option value="4">Economics—International ECON and Business</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
-
-<div class="form-group">
-    <label for="sel-intended-plans-minors-for-business-majors">Minors for Business Majors:</label>
-    <select class="form-control" id="sel-intended-plans-minors-for-business-majors" name="sel-intended-plans-minors-for-business-majors">
-        <option value="2">Finance—Investments/Banking</option>
-        <option value="5">Finance—Real Estate/Insurance</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-minors-for-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
-
-<div class="form-group">
-    <label for="sel-intended-plans-minors-for-non-business-majors">Minors for Non-Business Majors:</label>
-    <select class="form-control" id="sel-intended-plans-minors-for-non-business-majors" name="sel-intended-plans-minors-for-non-business-majors">
-        <option value="3">Information Systems</option>
-        <option value="6">Enterprise Systems</option>
-    </select>
-</div>
-<button id='btn-add-intended-plan-minors-for-non-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+<div class="panel panel-default">
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="sel-intended-plans-majors">Majors:</label>
+                <select class="form-control" id="sel-intended-plans-majors" name="sel-intended-plans-majors">
+                                            <option value="1">Economics—Business Economics</option>
+                                            <option value="4">Economics—International ECON and Business</option>
+                                    </select>
+            </div>
+            <button id='btn-add-intended-plan-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+        </div>
+    </div>
+        <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="sel-intended-plans-minors-for-business-majors">Minors for Business Majors:</label>
+                <select class="form-control" id="sel-intended-plans-minors-for-business-majors" name="sel-intended-plans-minors-for-business-majors">
+                                            <option value="2">Finance—Investments/Banking</option>
+                                            <option value="5">Finance—Real Estate/Insurance</option>
+                                    </select>
+            </div>
+            <button id='btn-add-intended-plan-minors-for-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+        </div>
+    </div>
+        <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="form-group">
+                <label for="sel-intended-plans-minors-for-non-business-majors">Minors for Non-Business Majors:</label>
+                <select class="form-control" id="sel-intended-plans-minors-for-non-business-majors" name="sel-intended-plans-minors-for-non-business-majors">
+                                            <option value="3">Information Systems</option>
+                                            <option value="6">Enterprise Systems</option>
+                                    </select>
+            </div>
+            <button id='btn-add-intended-plan-minors-for-non-business-majors' type="button" class="btn btn-success" onclick="addIntendedPlan()">Add</button>
+        </div>
+    </div>
 
 OPTIONS;
 
